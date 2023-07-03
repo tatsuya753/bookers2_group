4f28d3d96375ff66c06bae9e3f1a4f75cf669a7d
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-07-03 02:35:34 +0000

    add グループ機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
 end
-
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  
   resources :users, only: [:index, :show, :edit, :update] do
   resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
